a07dfccaf9799bb894b2677844c40aa8499eec9c
Piotr Patrzylas <piotrpatrzylas@gmail.com>
2020-01-31 23:44:18 +0000

    Notes update

---
+++ Algorithms.py
@@ -0,0 +1,36 @@
+"""
+Linear Search
+
+1. Linear search - two dimensional list of “n x m” elements
+
+Given a two dimensional list of “n x m” elements, write a function to
+count occurrences of a given key.
+The function should also return a matrix of the position of a key in
+the 2 dimensional list.
+For example, given the following list:
+Mylist = [[1,4,7],[4,5,2],[6,4,8]]
+and a key, e.g. key = 4
+The output should be
+3, [[0, 1], [1, 0], [2, 1]]
+# 3 occurrences of number 4 in positions: 0,1, 1,0 and 2,1
+What is the complexity of the linear search in the 2 dimensional
+array?
+"""
+
+"""
+Bubble Search
+
+Develop BubbleSort (hint)
+o It is the simplest sorting algorithm that works by repeatedly
+swapping the adjacent elements if they are in wrong order.
+o Implement a simple bubble sort algorithm!
+o What is the complexity of Bubble sort?
+"""
+
+"""
+Binary Search - recursive and iterative
+"""
+
+"""
+Interpolation Search
+"""

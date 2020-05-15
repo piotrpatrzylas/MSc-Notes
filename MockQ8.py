1ce2f2313ad77767d7cddcb6d16102fa95a92d5f
Piotr Patrzylas <piotrpatrzylas@gmail.com>
2020-05-15 21:50:53 +0100

    PoP Mock Test

--- MockQ8.py
+++ MockQ8.py
@@ -1,21 +1,24 @@
 def longest_sequence(s):
-    curr_char = s[0]
-    curr_count = 1
-    long_char = ""
-    temp = ""
-    long_count = 0
-    for i in range(len(s)-1):
-        if curr_char == s[i+1]:
-            curr_count += 1
-        else:
-            curr_char = s[i+1]
-            curr_count = 1
-        if curr_count > long_count:
-            long_char = curr_char
-            long_count = curr_count
-    return(long_char, long_count)
+    #provide implementation
 
+    i = s[0]
+    n = 0
 
-assert longest_sequence("dghhhmhmx") == ("h", 3)
-assert longest_sequence("dhkkhhkkkt") == ("k", 3)
-assert longest_sequence("abbbadddadd") == ("b", 3)
+    while n < len(s):
+        if s[n] != i: break
+        n += 1
+
+    done_until = n
+
+    while done_until < len(s):
+        cur_i = s[done_until]
+        cur_n = 0
+        while cur_n + done_until < len(s):
+            if s[cur_n + done_until] != cur_i: break
+            cur_n += 1
+
+        done_until += cur_n
+        if cur_n > n: n,i = cur_n, cur_i
+        if cur_n == n and cur_i < i: i = cur_i
+
+    return (i, n)
